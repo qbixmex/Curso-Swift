e32cb9121987393629494c879626451fc1c04cd4
Daniel González Briseño <70177108+qbixmex@users.noreply.github.com>
2025-12-02 19:24:58 +0000

    Operadores Básicos

---
+++ basics/02-operators.swift
@@ -0,0 +1,13 @@
+// Operadores Aritméticos
+
+let a: Int8 = 8;
+let b: Int8 = 2;
+
+print("\(a) + \(b) = \(a + b)")
+print("\(a) - \(b) = \(a - b)")
+print("\(a) x \(b) = \(a * b)")
+print("\(a) / \(b) = \(a / b)")
+
+// Module
+print("9 % 3 = \(9 % 3)")
+print("10 % 2 = \(10 % 2)")
