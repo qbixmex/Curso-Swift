fe8e2fa5a6640530f8cda1abba95ee53a8c0ae4d
Daniel González Briseño <70177108+qbixmex@users.noreply.github.com>
2025-12-03 12:59:16 -0600

    Repeat

--- basics/09-while-loop.swift
+++ basics/09-while-loop.swift
@@ -12,3 +12,18 @@ while (index <= 10) {
 }
 
 print(output)
+
+// Con Repeat
+var index = 0
+var output = ""
+
+repeat {
+    if (index != 10) {
+        output += "\(index), "
+    } else {
+        output += "\(index)"
+    }
+    index += 1
+} while (index <= 10)
+
+print(output)
